54284887b81039f941aca4091654bb17d7b3952d
Mohit Panchariya <iammohitpanchariya@gmail.com>
2023-10-14 01:46:33 +0530

    Implement file patching logic

---
+++ poc/patch.py
@@ -0,0 +1,58 @@
+from functools import partial
+
+class Patch:
+
+    #Delta File Command Representations
+    END_COMMAND = 0
+    LITERAL_COMMAND = 1
+    COPY_COMMAND = 2
+
+    #Size in bytes
+    COMMAND_SIZE = 1
+
+    def __init__(self):
+        pass
+
+    def patchFile(self, deltaFilePath, basisFilePath, outFilePath):
+
+        with (
+            open(deltaFilePath, "rb") as deltaFile,
+            open(basisFilePath, "rb") as basisFile,
+            open(outFilePath, "wb") as outFile
+        ):
+            
+            for command in iter(partial(deltaFile.read, self.COMMAND_SIZE), b''):
+
+                command =  int.from_bytes(command, byteorder="big")
+                #Return if reached end of file
+                if command == self.END_COMMAND:
+                    return
+                
+                elif command == self.COPY_COMMAND:
+                    #Size of block index is represented using 4 bytes in the delta file
+                    blockIndex = int.from_bytes(deltaFile.read(4), byteorder="big")
+                    #Number of bytes to read is represented using 4 bytes in the delta file
+                    bytesToRead = int.from_bytes(deltaFile.read(4), byteorder="big")
+
+                    #Seek to the block to be read
+                    basisFile.seek(1024 * blockIndex)
+
+                    block = basisFile.read(bytesToRead)
+                    
+                    #Write bytes to the out file
+                    outFile.write(block)
+                
+                elif command == self.LITERAL_COMMAND:
+                    '''
+                    The number of literals to be copied from the delta file is
+                    represented using 4 bytes.
+                    '''
+                    bytesToRead = int.from_bytes(deltaFile.read(4), byteorder="big")
+                    #Read literals from the delta file
+                    block = deltaFile.read(bytesToRead)
+
+                    #Write literals to the out file
+                    outFile.write(block)
+
+                else:
+                    raise Exception("Undefined Command in delta file.")
